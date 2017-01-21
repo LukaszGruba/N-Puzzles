d0d45451c7d9d7376f79f0d90d6870bf33841fdd
LukaszGruba <lukgru@gmail.com>
2017-01-21 18:09:40 +0100

    main class

---
+++ src/main/java/com/lukgru/npuzzles/NPuzzlesSolver.java
@@ -0,0 +1,11 @@
+package com.lukgru.npuzzles;
+
+/**
+ * Created by Lukasz on 21.01.2017.
+ */
+public class NPuzzlesSolver {
+
+    public static void main(String[] args) {
+        System.out.println("N-Puzzles Sover started");
+    }
+}
