bb03a46d710f7a41f56a0eea9791b62ab8cdc7a2
LukaszGruba <lukgru@gmail.com>
2017-01-22 09:15:30 +0100

    SolutionPrinter init imple

---
+++ src/main/java/com/lukgru/npuzzles/SolutionPrinter.java
@@ -0,0 +1,20 @@
+package com.lukgru.npuzzles;
+
+import java.io.PrintStream;
+import java.util.List;
+
+/**
+ * Created by Lukasz on 22.01.2017.
+ */
+public class SolutionPrinter {
+
+    private PrintStream printStream;
+
+    public SolutionPrinter(PrintStream printStream) {
+        this.printStream = printStream;
+    }
+
+    public void print(List<Move> solution) {
+        //TODO: implement
+    }
+}
