63e1e2ce3a14bd4f41771488d50fb5138f9f2aa0
LukaszGruba <lukgru@gmail.com>
2017-01-22 09:15:05 +0100

    BoardBuilder init impl

---
+++ src/main/java/com/lukgru/npuzzles/BoardBuilder.java
@@ -0,0 +1,16 @@
+package com.lukgru.npuzzles;
+
+import com.lukgru.npuzzles.model.Board;
+
+/**
+ * Created by Lukasz on 22.01.2017.
+ */
+public class BoardBuilder {
+    public BoardBuilder(String[] args) {
+    }
+
+    public Board build() {
+        //TODO: implement
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+}
