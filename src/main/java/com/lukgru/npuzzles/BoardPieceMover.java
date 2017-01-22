64e283bcebd7e5c522dbdfe5e0b65aece4f90212
LukaszGruba <lukgru@gmail.com>
2017-01-22 09:17:37 +0100

    BoardPieceMover init impl

---
+++ src/main/java/com/lukgru/npuzzles/BoardPieceMover.java
@@ -0,0 +1,29 @@
+package com.lukgru.npuzzles;
+
+import com.lukgru.npuzzles.model.Board;
+
+/**
+ * Created by Lukasz on 22.01.2017.
+ */
+public class BoardPieceMover {
+
+    public boolean fillGapByPieceFromLeft(Board board) {
+        //TODO: implement
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    public boolean fillGapByPieceFromRight(Board board) {
+        //TODO: implement
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    public boolean fillGapByPieceFromUp(Board board) {
+        //TODO: implement
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    public boolean fillGapByPieceFromDown(Board board) {
+        //TODO: implement
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+}
---
+++ src/test/java/com/lukgru/npuzzles/BoardPieceMoverTest.java
@@ -0,0 +1,8 @@
+package com.lukgru.npuzzles;
+
+/**
+ * Created by Lukasz on 22.01.2017.
+ */
+public class BoardPieceMoverTest {
+    //TODO: add tests
+}
