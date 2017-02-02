d8b2c7a1fc2bbc2e054bf4c9d31c10e45c76ea76
LukaszGruba <lukgru@gmail.com>
2017-02-02 21:50:14 +0100

    Board.getBoardArray returns copy of elements

--- src/main/java/com/lukgru/npuzzles/model/Board.java
+++ src/main/java/com/lukgru/npuzzles/model/Board.java
@@ -17,10 +17,36 @@ public class Board {
     }
 
     public Piece[][] getBoardArray() {
-        return boardArray;
+        Piece[][] newPieces = new Piece[boardArray.length][boardArray[0].length];
+        piecesStream().forEach(piece -> {
+            String value = piece.getValue();
+            int x = piece.getPosition().getX();
+            int y = piece.getPosition().getY();
+            newPieces[y][x] = new Piece(value, new Position(x, y));
+        });
+        return newPieces;
     }
 
     public Stream<Piece> piecesStream() {
         return Arrays.stream(boardArray).flatMap(Arrays::stream);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Board board = (Board) o;
+
+        return Arrays.deepEquals(boardArray, board.boardArray);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(boardArray);
     }
 }
