72ca3e28cb5ab48879329c99336c55b23808d33a
LukaszGruba <lukgru@gmail.com>
2017-02-05 22:07:51 +0100

    checkstyle

--- src/main/java/com/lukgru/npuzzles/algorithm/SolvabilityVerification.java
+++ src/main/java/com/lukgru/npuzzles/algorithm/SolvabilityVerification.java
@@ -1,10 +1,10 @@
 package com.lukgru.npuzzles.algorithm;
 
+import static com.lukgru.npuzzles.model.Piece.EMPTY;
+
 import com.lukgru.npuzzles.model.Board;
 import com.lukgru.npuzzles.model.Piece;
 import com.lukgru.npuzzles.model.Position;
-
-import static com.lukgru.npuzzles.model.Piece.EMPTY;
 
 /**
  * Created by Lukasz on 05.02.2017.
@@ -21,11 +21,10 @@ public class SolvabilityVerification {
         if (inversions == 0) {
             return true;
         }
-        int N = board.getBoardArray().length;
-        if (isOdd(N) && isEven(inversions)) {
+        int boardSize = board.getBoardArray().length;
+        if (isOdd(boardSize) && isEven(inversions)) {
             solvable = true;
-        }
-        else if (isEven(N)) {
+        } else if (isEven(boardSize)) {
             int emptyPositionFromDown = emptyPositionFromDown(board);
             if ((isEven(inversions) && isOdd(emptyPositionFromDown))
                     || (isOdd(inversions) && isEven(emptyPositionFromDown))) {
@@ -43,11 +42,11 @@ public class SolvabilityVerification {
 
     private int countInversions(Board board) {
         Piece[][] pieces = board.getBoardArray();
-        int N = pieces.length;
-        int[] all = new int[(N * N) -1];
+        int boardSize = pieces.length;
+        int[] all = new int[(boardSize * boardSize) - 1];
         int index = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
                 String value = pieces[i][j].getValue();
                 if (!EMPTY.equals(value)) {
                     all[index] = Integer.parseInt(value);
@@ -58,7 +57,7 @@ public class SolvabilityVerification {
 
         int inversions = 0;
         for (int i = 0; i < all.length; i++) {
-            for (int j = i+1; j < all.length; j++) {
+            for (int j = i + 1; j < all.length; j++) {
                 if (all[j] < all[i]) {
                     inversions++;
                 }
