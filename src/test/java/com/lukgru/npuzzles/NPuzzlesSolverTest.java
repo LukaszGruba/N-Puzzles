d515d69305b49df5195ff06390e88408e359c7f1
LukaszGruba <lukgru@gmail.com>
2017-02-05 17:55:55 +0100

    removed non-sqare board tests as non-square is not supported

--- src/test/java/com/lukgru/npuzzles/NPuzzlesSolverTest.java
+++ src/test/java/com/lukgru/npuzzles/NPuzzlesSolverTest.java
@@ -4,7 +4,6 @@ import com.lukgru.npuzzles.heuristic.Heuristic;
 import com.lukgru.npuzzles.heuristic.ManhattanHeuristic;
 import com.lukgru.npuzzles.io.InputParser;
 import com.lukgru.npuzzles.model.Board;
-import com.lukgru.npuzzles.model.Piece;
 import com.lukgru.npuzzles.model.Step;
 import org.junit.Test;
 
@@ -45,51 +44,21 @@ public class NPuzzlesSolverTest {
         assertEquals(target, solution.get(solution.size() - 1).getState());
     }
 
-    @Test
+    @Test(expected = RuntimeException.class)
     public void shouldNotSolve2x2WhenNotPossible() {
         //given
         Board board = new InputParser().parse(new String[]{
-                "1 " +EMPTY,
+                "1 " + EMPTY,
                 "2 3"
         });
         Board target = new InputParser().parse(new String[]{
                 "1 2",
-                "3 "+EMPTY
+                "3 " + EMPTY
         });
 
         //when
-        List<Step> solution = solver.solve(board, target);
-        System.out.println("SOLUTION");
-        solution.stream().forEach(System.out::print);
-
-        //then
-        assertNotNull(solution);
-        assertEquals(0, solution.size());
+        solver.solve(board, target);
     }
-
-    @Test
-    public void shouldSolve2x3WhenPossible() {
-        //given
-        Board board = new InputParser().parse(new String[]{
-                "1 " +EMPTY + " 5",
-                "2 4 3"
-        });
-        Board target = new InputParser().parse(new String[]{
-                "1 2 3",
-                "4 5 "+EMPTY
-        });
-
-        //when
-        List<Step> solution = solver.solve(board, target);
-        System.out.println("SOLUTION");
-        solution.stream().forEach(System.out::print);
-
-        //then
-        assertNotNull(solution);
-        assertEquals(board, solution.get(0).getState());
-        assertEquals(target, solution.get(solution.size() - 1).getState());
-    }
-
 
     @Test
     public void shouldSolve3x3WhenPossible() {
@@ -117,31 +86,6 @@ public class NPuzzlesSolverTest {
     }
 
     @Test
-    public void shouldSolve3x4WhenPossible() {
-        //given
-        Board board = new InputParser().parse(new String[]{
-                "6 2 " + EMPTY + " 3",
-                "7 11 10 1",
-                "9 4 5 8"
-        });
-        Board target = new InputParser().parse(new String[]{
-                "1 2 3 4",
-                "5 6 7 8",
-                "9 10 11 " + EMPTY
-        });
-
-        //when
-        List<Step> solution = solver.solve(board, target);
-        System.out.println("SOLUTION");
-        solution.stream().forEach(System.out::print);
-
-        //then
-        assertNotNull(solution);
-        assertEquals(board, solution.get(0).getState());
-        assertEquals(target, solution.get(solution.size() - 1).getState());
-    }
-
-    @Test
     public void shouldSolve4x4WhenPossible() {
         //given
         Board board = new InputParser().parse(new String[]{
@@ -155,33 +99,6 @@ public class NPuzzlesSolverTest {
                 "5 6 7 8",
                 "9 10 11 12",
                 "13 14 15 " + EMPTY
-        });
-
-        //when
-        List<Step> solution = solver.solve(board, target);
-        System.out.println("SOLUTION");
-        solution.stream().forEach(System.out::print);
-
-        //then
-        assertNotNull(solution);
-        assertEquals(board, solution.get(0).getState());
-        assertEquals(target, solution.get(solution.size() - 1).getState());
-    }
-
-    @Test
-    public void shouldSolve4x5WhenPossible() {
-        //given
-        Board board = new InputParser().parse(new String[]{
-                "14 3 8 1 15",
-                "7 2 11 19 4",
-                "16 9 " + EMPTY + " 12 10",
-                "17 13 5 6 18"
-        });
-        Board target = new InputParser().parse(new String[]{
-                "1 2 3 4 5",
-                "6 7 8 9 10",
-                "11 12 13 14 15",
-                "16 17 18 19 " + EMPTY,
         });
 
         //when
